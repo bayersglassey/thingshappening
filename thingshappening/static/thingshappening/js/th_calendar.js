5c206d170b6ac7a7aaf22efa9e35ee048b84f6e0
Ben Ayers-Glassey <bayersglassey@gmail.com>
2018-01-22 16:37:05 -0800

    th_calendar.js: moved make_month into CalendarMonth. Added basic add_events

--- thingshappening/static/thingshappening/js/th_calendar.js
+++ thingshappening/static/thingshappening/js/th_calendar.js
@@ -1,8 +1,19 @@
 
 window.th_calendar = (function($){
     var th_calendar = {};
+    th_calendar.CalendarMonth = CalendarMonth;
 
-    th_calendar.make_month = function(year, month){
+    function CalendarMonth(year, month){
+        /*
+            Usage example:
+
+                var jan = new CalendarMonth(2018, 0);
+                jan.add_events([
+                    {...}
+                ]);
+                document.body.append(jan.elem_table);
+        */
+
         /* E.g. for Jan 2018, year = 2018; month = 0 */
         var d = moment([year, month]);
         var n_dates = d.daysInMonth();
@@ -56,6 +67,7 @@ window.th_calendar = (function($){
                 var elem_td = document.createElement('td');
                 tds[k] = elem_td;
                 elem_td.setAttribute('class', k % 2 == 0? 'zebra1': 'zebra2');
+                elem_td.th_events = [];
                 elem_tr.append(elem_td);
             }
             elem_tbody.append(elem_tr);
@@ -71,15 +83,84 @@ window.th_calendar = (function($){
             elem_daynumber.textContent = i + 1;
             elem_daynumber.setAttribute('class', 'calendar-daynumber');
 
+            td.elem_daynumber = elem_daynumber;
             td.append(elem_daynumber);
         }
 
-        return elem_table;
+        /* Set attributes of 'this': */
+        this.d = d;
+        this.n_dates = n_dates;
+        this.date_offset = date_offset;
+        this.n_rows = n_rows;
+        this.n_cols = n_cols;
+        this.tds = tds;
+        this.elem_table = elem_table;
+    }
+
+    CalendarMonth.prototype.add_events = function(events){
+        var n_events = events.length;
+        for(var i = 0; i < n_events; i++){
+            var event = events[i];
+
+            /* Figure out start/end indices into this.tds */
+            var start = moment(event.start);
+            var end = moment(event.end);
+            var start_date = start.date();
+            var end_date = end.date();
+            var start_td_i = this.date_offset + start_date;
+            var end_td_i = this.date_offset + end_date;
+
+            /* Add event to <td>s for the dates it covers */
+            var tds = this.tds;
+            for(var td_i = start_td_i; td_i <= end_td_i; td_i++){
+                var td = tds[td_i];
+                td.th_events.push(td);
+            }
+        }
+
+        var tds = this.tds;
+        var n_tds = tds.length;
+        for(var i = 0; i < n_tds; i++){
+            var td = tds[i];
+            var events = td.th_events;
+            if(events.length > 0){
+                td.elem_daynumber.style.fontWeight = "bold";
+            }
+        }
     }
 
     $(document).ready(function(){
-        $("#main").append(th_calendar.make_month(2018, 0));
-        $("#main").append(th_calendar.make_month(2018, 1));
+        var jan_events = [
+            {
+                "id": 1,
+                "title": "Going to the Park",
+                "description": "Join us! We're all going to the park today.",
+                "user_id": 1,
+                "start": "2018-01-22T17:42:04.337652Z",
+                "end": "2018-01-22T17:41:42.044126Z"
+            },
+            {
+                "id": 29,
+                "title": "Professional Blag",
+                "description": "Test event",
+                "user_id": 29,
+                "start": "2020-01-08T18:15:00Z",
+                "end": "2020-01-10T12:00:00Z"
+            },
+            {
+                "id": 30,
+                "title": "Cooking Party",
+                "description": "Test event",
+                "user_id": 53,
+                "start": "2019-01-04T05:45:00Z",
+                "end": "2020-01-21T09:00:00Z"
+            }
+        ];
+        var jan = new CalendarMonth(2018, 0);
+        var feb = new CalendarMonth(2018, 1);
+        jan.add_events(jan_events);
+        $("#main").append(jan.elem_table);
+        $("#main").append(feb.elem_table);
     });
 
 

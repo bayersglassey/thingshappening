afd6b3e7c5c5f91a7b3722c79f1e46c56c088928
Ben Ayers-Glassey <bayersglassey@gmail.com>
2018-01-24 21:49:40 -0800

    Installed django-filters, django-extensions

--- thingshappening/settings.py
+++ thingshappening/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'thingshappening',
+    'django_filters',
+    'django_extensions',
     'rest_framework',
 ]
 
